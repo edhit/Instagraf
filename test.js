5fe927f77a3866922efcf786c001409ccc3a4f24
edhit <46953444+edhit@users.noreply.github.com>
2025-01-31 23:42:17 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const instagramGetUrl = require('instagram-url-direct');
 
 (async () => {
-    const url = 'https://www.instagram.com/reel/КОД_ВИДЕО/';
+    const url = 'https://www.ddinstagram.com/reel/DEiT7yNMx9F/?igsh=OGlubXdvcTAwNndt';
     try {
         const result = await instagramGetUrl(url);
         console.log('Результат:', result);
