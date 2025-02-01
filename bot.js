de49eac091b63746784016803531d9f2fbb1cb61
Mona Lisa <Mona Lisa>
2025-02-01 09:22:30 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   bot.js
    	new file:   package-lock.json

--- bot.js
+++ bot.js
@@ -1,38 +1,40 @@
-const { Telegraf } = require('telegraf');
+const {
+    Telegraf
+} = require('telegraf');
 const instagramGetUrl = require('instagram-url-direct');
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
+// const axios = require('axios');
+// const fs = require('fs');
+// const path = require('path');
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
-function normalizeInstagramUrl(url) {
-    if (url.includes('ddinstagram.com')) {
-        const reelCode = url.split('/reel/')[1].split('/')[0];
-        return `https://www.instagram.com/reel/${reelCode}/`;
-    }
-    return url;
-}
+// function normalizeInstagramUrl(url) {
+//     if (url.includes('ddinstagram.com')) {
+//         const reelCode = url.split('/reel/')[1].split('/')[0];
+//         return `https://www.instagram.com/reel/${reelCode}/`;
+//     }
+//     return url;
+// }
 
-const downloadVideo = async (videoUrl, filePath) => {
-    try {
-        const writer = fs.createWriteStream(filePath);
-        const response = await axios.get(videoUrl, { responseType: 'stream' });
+// const downloadVideo = async (videoUrl, filePath) => {
+//     try {
+//         const writer = fs.createWriteStream(filePath);
+//         const response = await axios.get(videoUrl, { responseType: 'stream' });
 
-        return new Promise((resolve, reject) => {
-            response.data.pipe(writer);
-            writer.on('finish', resolve);
-            writer.on('error', (err) => {
-                console.error('Ошибка при записи видео:', err.message);
-                reject(err);
-            });
-        });
-    } catch (err) {
-        console.error('Ошибка при загрузке видео:', err.message);
-        throw err;
-    }
-};
+//         return new Promise((resolve, reject) => {
+//             response.data.pipe(writer);
+//             writer.on('finish', resolve);
+//             writer.on('error', (err) => {
+//                 console.error('Ошибка при записи видео:', err.message);
+//                 reject(err);
+//             });
+//         });
+//     } catch (err) {
+//         console.error('Ошибка при загрузке видео:', err.message);
+//         throw err;
+//     }
+// };
 
 const isValidUrl = (string) => {
     try {
@@ -51,20 +53,25 @@ bot.on('text', async (ctx) => {
     }
 
     const normalizedUrl = url
-    console.log(url)
 
     try {
         const result = await instagramGetUrl(normalizedUrl);
-console.log(result)
+
         const videoUrl = result.url_list[0];
-        const videoPath = path.join(__dirname, 'video.mp4');
+        const caption = result.post_info['caption']
+        // const videoPath = path.join(__dirname, 'video.mp4');
         try {
-            await downloadVideo(videoUrl, videoPath);
-            await ctx.replyWithVideo({ source: videoPath });
+            // await downloadVideo(videoUrl, videoPath);
+            await ctx.replyWithVideo({
+                url: videoUrl
+            }, {
+                caption: caption,
+                reply_to_message_id: ctx.message.message_id
+            });
         } finally {
-            if (fs.existsSync(videoPath)) {
-                fs.unlinkSync(videoPath);
-            }
+            // if (fs.existsSync(videoPath)) {
+            //     fs.unlinkSync(videoPath);
+            // }
         }
     } catch (error) {
         console.error('Ошибка при обработке ссылки:', normalizedUrl, error);
